1d87a4ad0a55c139d637ca01a578045f781c4dfc
Pavel Golovin <golovinp2014@mail.ru>
2024-09-18 18:19:22 +0300

    fix: layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import "../styles/sds/index.scss";
+import "../../styles/sds/index.scss";
 import { Inter } from "next/font/google";
 import localFont from 'next/font/local'
-import "../styles/index.scss";
+import "../../styles/index.scss";
 import { Footer, Header } from "@/components/ui/compositions";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ClerkProvider } from "@clerk/nextjs";
 import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
+import {getMessages, unstable_setRequestLocale} from 'next-intl/server';
+import { routing } from "@/i18n/routing";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 
@@ -20,7 +21,7 @@ const Muller = localFont({
   variable: '--font-muller',
   src: [
     {
-      path: '../fonts/MullerBoldItalic.woff2',
+      path: '../../fonts/MullerBoldItalic.woff2',
       weight: '700',
       style: 'italic',
     }
@@ -31,12 +32,16 @@ const GothamPro = localFont({
   variable: '--font-gotham',
   src: [
     {
-      path: '../fonts/GothamPro-BoldItalic.woff2',
+      path: '../../fonts/GothamPro-BoldItalic.woff2',
       weight: '700',
       style: 'italic',
     }
   ],
 })
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({locale}));
+}
 
 export default async function RootLayout({
   children,
@@ -45,7 +50,7 @@ export default async function RootLayout({
   children: React.ReactNode;
   params: {locale: string};
 }>) {
-
+  unstable_setRequestLocale(locale);
   const messages = await getMessages();
   return (
     <ClerkProvider>
