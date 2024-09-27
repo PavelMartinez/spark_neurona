e6e895d07b3977608bebf79947ed39a65a7c3007
Pavel Golovin <golovinp2014@mail.ru>
2024-09-27 05:12:34 +0300

    fix: locale

--- src/app/[locale]/sign-in/page.ts
+++ src/app/[locale]/sign-in/page.ts
@@ -2,10 +2,10 @@ import { redirect } from "next/navigation";
 
 export default async function SignIn({
     searchParams,
-    locale
+    params: { locale }
 }: {
     searchParams?:{ [key: string]: string | undefined };
-    locale: string;
+    params: { locale: string };
 }) {
     redirect(`/${locale}/?sign-in${searchParams?.redirect ? "&redirect=" + searchParams.redirect : ""}`);
 }
